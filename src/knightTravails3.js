f0a8ff27c2978108e490286b87d0b42737fc14fe
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2025-03-05 20:23:00 -0500

    Add prevMove function to complete the knightMoves function

--- src/knightTravails3.js
+++ src/knightTravails3.js
@@ -6,7 +6,6 @@ function addNode(node) {
 
 function addEdge(node1, node2) {
   adjacentcyList.get(node1).push(node2);
-  adjacentcyList.get(node2).push(node1);
 }
 
 for (let i = 0; i < 8; i++) {
@@ -37,6 +36,58 @@ for (let key of adjacentcyList.keys()) {
   setValidMoves(key);
 }
 
-function knightMoves(start, end) {}
+function findPrevMove(start, end) {
+  start = start.toString();
+  end = end.toString();
+  if (start === end) return;
+  let nextLayer = [start];
+  let queue = nextLayer.slice();
+  let visited = new Set();
 
-console.log(adjacentcyList);
+  while (queue.length > 0) {
+    for (let node of queue) {
+      let edges = adjacentcyList.get(node);
+      for (let edge of edges) {
+        if (edge === end) {
+          return node;
+        } else {
+          if (!visited.has(edge)) {
+            visited.add(edge);
+            nextLayer.push(edge);
+          }
+        }
+      }
+    }
+    queue = nextLayer;
+    nextLayer = [];
+  }
+}
+
+function knightMoves(start, end) {
+  if (
+    start[0] > 7 ||
+    start[0] < 0 ||
+    start[1] > 7 ||
+    start[1] < 0 ||
+    end[0] > 7 ||
+    end[0] < 0 ||
+    end[1] > 7 ||
+    end[1] < 0
+  ) {
+    console.log("must pick values between 0-7");
+    return null;
+  } else if (start.toString() === end.toString()) {
+    return [start.toString()];
+  }
+  let fullPath = [end.toString()];
+  let prevMove = [findPrevMove(start, end)];
+
+  while (prevMove.toString() !== start.toString()) {
+    fullPath.unshift(prevMove);
+    prevMove = [findPrevMove(start, prevMove)];
+  }
+  fullPath.unshift([start.toString()]);
+  return fullPath;
+}
+
+console.log(knightMoves([4, 1], [7, 5]));
